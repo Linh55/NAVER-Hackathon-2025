d402a88fa6b82c91c19e242256499cc976c6abbb
Linh55 <khlinh41@gmail.com>
2025-09-14 09:59:06 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,4 @@
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 >>>>>>> aff305642fc91ffe31cbc44b06800b7a53ceee1b
