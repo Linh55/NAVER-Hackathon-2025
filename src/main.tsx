0908d612c46b5cda10b616e8cdcb2bb07b96737e
Linh55 <khlinh41@gmail.com>
2025-09-14 09:59:15 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
->>>>>>> aff305642fc91ffe31cbc44b06800b7a53ceee1b
+
 import './index.css'
 import App from './App.tsx'
 
